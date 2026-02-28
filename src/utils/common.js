8ee223b35f02b310b8ff5828b8346ecb379e40df
Debaranjan <debaranjan.lenka2006@gmail.com>
2026-02-28 10:55:52 +0530

    Clean project structure

---
+++ src/utils/common.js
@@ -0,0 +1,18 @@
+export const sliderSettings = {
+    slidersPerView: 1,
+    spaceBetween: 50,
+    breakpoints: {
+        480: {
+            slidersPerView: 1
+        },
+        600: {
+            slidersPerView: 2
+        },
+        750: {
+            slidersPerView: 3
+        },
+        1100: {
+            slidersPerView: 4 
+        }
+    }
+}
